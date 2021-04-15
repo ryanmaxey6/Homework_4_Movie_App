a4aec7672ae6d9959e9bbc3ca7378861d62024ea
ryanmaxey6 <ryanmaxey6@vt.edu>
2021-04-14 23:59:04 -0400

    Added light and dark mode

--- app/src/main/java/com/example/homework4/MainActivity.java
+++ app/src/main/java/com/example/homework4/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.homework4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -28,12 +33,45 @@ public class MainActivity extends AppCompatActivity {
 
     List<Movie> movies;
 
+    Switch switchDNMode;
+    SharedPreferences sharedPreferences = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
         movies = new ArrayList<>();
+        switchDNMode = findViewById(R.id.switchDNMode);
+
+        sharedPreferences = getSharedPreferences("night", 0);
+        Boolean defaultNight = sharedPreferences.getBoolean("night_mode", true);
+        if (defaultNight)
+        {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchDNMode.setChecked(true);
+        }
+
+        switchDNMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchDNMode.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", true);
+                    editor.commit();
+                }
+                else
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchDNMode.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", false);
+                    editor.commit();
+                }
+            }
+        });
 
         // Create the adapter
         MovieAdapter movieAdapter = new MovieAdapter(this, movies);
